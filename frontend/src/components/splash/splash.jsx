ec7a77ef749204700e34a1bd2fa0c6e055af1d25
acheung-94 <acheung@ucdavis.edu>
2024-03-31 00:13:56 -0700

    complete splash page

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -1,21 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import './splash.css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareGithub, faLinkedin, faDiscord } from '@fortawesome/free-brands-svg-icons'
 const Splash = () => {
-
+    const navigate = useNavigate()
     return(
         <>
-        <div className="splash-page-wrapper">
-            <div className="splash-navbar">
-                <Link to={`/login`}>Login</Link>
+            <div className="splash-page-wrapper">
+            <div className="welcome-wrapper">
+                <div className="splash-navbar">
+                    <span className="logo"><FontAwesomeIcon icon={faDiscord} /> Dischord</span>
+                    <div className="links">
+                        <a href="https://github.com/acheung-94"><FontAwesomeIcon icon={faSquareGithub} /> Github </a>
+                        <a href="https://www.linkedin.com/in/andrea-cheung-b9b5072b2/"><FontAwesomeIcon icon={faLinkedin} /> LinkedIn  </a>
+                        <a href="https://discord.com"> The Real Discord  </a>
+                    </div>
+                    <button className="to-login" onClick={()=>navigate('/login')}>Login</button>
+                </div>
+                <div className="welcome-msg">
+                    <h1> Imagine a place... </h1>
+                    <p>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
+                    <p>Well... you've found it.</p>
+                    <h4>Welcome to Dischord.</h4>
+                </div>
+                <div className="welcome-footer"></div>   
+              </div>     
             </div>
-            <div className="welcome-msg">
-                <h1> Imagine a place... </h1>
-                <p>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
-            </div>          
-        </div>
+            
         </>
-
     )
 }
 
