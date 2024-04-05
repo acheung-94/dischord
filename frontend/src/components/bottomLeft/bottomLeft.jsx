636be36d40b1ec4d0fc2cc98fcff1aa79c96c9cb
acheung-94 <acheung@ucdavis.edu>
2024-04-04 19:24:44 -0700

    modularize usericons & add avatars

--- frontend/src/components/bottomLeft/bottomLeft.jsx
+++ frontend/src/components/bottomLeft/bottomLeft.jsx
@@ -1,21 +1,15 @@
 import { useSelector, useDispatch } from 'react-redux';
 import './bottomLeft.css'
 import { logoutUser, selectCurrentUser } from '../../store/sessionReducer';
+import UserIcon from '../userIcon/userIcon';
 
 const BottomLeft = () => {
     const currentUser = useSelector(selectCurrentUser)
     const dispatch = useDispatch()
     return(
         <div className='bottom-left'>
-            <div className="user-icon">
-                <img className="avatar" src="/src/assets/icons/avatar-gold.png" />
-                <img className='status' src="/src/assets/icons/icon-on.png" />
-                <div className="user-icon-text">
-                    <h4>{currentUser.username}</h4>
-                    <p>Online</p>
-                </div>
-            
-            </div>
+            <UserIcon user={currentUser} type="bottom-left" />
+
             <div className='user-options'
                     onClick={() => dispatch(logoutUser())}>
                 <img src="/src/assets/icons/leaving.png" />
--- frontend/src/components/userIcon/userIcon.jsx
+++ frontend/src/components/userIcon/userIcon.jsx
@@ -1,7 +1,26 @@
-const Banana = ({user}) => {
+import './userIcon.css'
+
+const UserIcon = ({user, type}) => {
+    const defaultColors = ["blurple", "gold", "gray", "green", "magenta", "red"]
+
+    const assignImage = () => {
+        if (user.avatarUrl){
+            return user.avatarUrl
+        }else{
+            let color = defaultColors[Math.floor(Math.random() * 5)]
+            return `/src/assets/icons/avatar-${color}.png`
+        }
+    }
     return(
-        <h1>Banana...</h1>
+        <div className="user-icon">
+                <img className="avatar" src={assignImage()} />
+                <img className='status' src="/src/assets/icons/icon-on.png" />
+                <div className="user-icon-text">
+                    <h4>{user.username}</h4>
+                    { type === 'bottom-left' && (<p>"status"</p>)}
+                </div>
+        </div>
     )
 }
 
-export default Banana; 
+export default UserIcon; 
