896ee833690ebf003c90cd7af21ce625a91ba2dc
acheung-94 <acheung@ucdavis.edu>
2024-04-06 11:03:53 -0700

    format message item, attempt scrollbars

--- frontend/src/components/messageItem/messageItem.jsx
+++ frontend/src/components/messageItem/messageItem.jsx
@@ -4,9 +4,14 @@ const MessageItem = ({message}) => {
 
     return(
         <div className='message-item'>
-            <p className='message-time'>{message.timestamp}</p>
-            <h4 className='message-author'> {message.author} </h4>
-            <p className='message-body'>{message.body}</p>
+            
+            <p className='message-body'>
+                <div className='message-header'>
+                    <p className='message-time'>{message.timestamp}</p>
+                    <h4 className='message-author'> {message.author} </h4>
+                </div>  
+            {message.body}
+            </p>
         </div>
     )
 }
--- frontend/src/components/messageList/messageList.jsx
+++ frontend/src/components/messageList/messageList.jsx
@@ -3,19 +3,21 @@ import MessageItem from '../messageItem/messageItem';
 const MessageList = ( {messages, channelId, channel}) => {
 
     return(
-        <div className="message-list">
-            <div className="new-channel-thing">
-                <img src="/src/assets/icons/channelTextWhite.png" />
-            </div>
-            <h1 className='message-list-welcome'>
-                Welcome to #{channel.name}!
-            </h1>
-            <p>This is the start of the #{channel.name} channel.</p>
-            <span className="list-separator"></span>
-            { messages.map( message => (
-                <MessageItem message={message}/>
-            )) }
+        <div className="message-list-wrapper">
+            <div className="message-list">
+                <div className="new-channel-thing">
+                    <img src="/src/assets/icons/channelTextWhite.png" />
+                </div>
+                <h1 className='message-list-welcome'>
+                    Welcome to #{channel.name}!
+                </h1>
+                <p>This is the start of the #{channel.name} channel.</p>
+                <span className="list-separator"></span>
+                { messages.map( message => (
+                    <MessageItem message={message}/>
+                )) }
 
+            </div>
         </div>
     )
 }
