cb62834913cf835e41e0dd8793b6a8778c725002
acheung-94 <acheung@ucdavis.edu>
2024-04-07 11:25:40 -0700

    add formatted date for dynamic message separator action, fix scrolling

--- app/views/api/messages/index.json.jbuilder
+++ app/views/api/messages/index.json.jbuilder
@@ -3,6 +3,7 @@
         json.extract! message, :id, :body, :author_id, :channel_id, :created_at, :updated_at
         json.author message.author.username
         json.timestamp message.created_at.to_time.strftime('%l:%M %p')
+        json.date message.created_at.to_time.localtime.strftime('%B%_e, %Y')
         if message.images.attached?
             json.array! url_for(message.images) #really not sure if this is the right syntax
         end
--- frontend/src/components/messageList/messageList.jsx
+++ frontend/src/components/messageList/messageList.jsx
@@ -12,14 +12,32 @@ const MessageList = ( {messages, currentUser, channel}) => {
                 <h1 className='message-list-welcome'>
                     Welcome to #{channel.name}!
                 </h1>
-                <p>This is the start of the #{channel.name} channel.</p>
+                <p className='message-start'>This is the start of the #{channel.name} channel.</p>
                 <span className="list-separator"></span>
-                { messages.map( message => (
-                    <MessageItem key = {message.id} 
-                    message={message} 
-                    currentUser={currentUser} 
-                    channel={channel}/>
-                )) }
+                { messages.map( (message, idx) => {
+                    if (idx === 0 || message.date !== messages[idx - 1].date){
+                        return (
+                            <>
+                            <span className="list-separator"> 
+                                <span></span>
+                                <div>{message.date} </div>
+                                <span></span>
+                            </span>
+                            <MessageItem key = {message.id} 
+                                            message={message} 
+                                            currentUser={currentUser} 
+                                            channel={channel}/>
+                            </>
+                        )
+                    }else{
+                        return (
+                            <MessageItem key = {message.id} 
+                                            message={message} 
+                                            currentUser={currentUser} 
+                                            channel={channel}/>
+                        )
+                    }
+                }) }
 
             </div>
         </div>
