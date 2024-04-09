869068a0510d493cdb04a982a749687b56627ab1
acheung-94 <acheung@ucdavis.edu>
2024-04-09 11:46:18 -0700

    styling update for user icons

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,12 @@
 class Api::UsersController < ApplicationController
     wrap_parameters include: User.attribute_names + ['password'] #allows nesting of password under :user key, for the rest, Rails will find a column and automatically nest any matching attributes under 'user'.
     before_action :require_logged_out, only: [:create]
-
+    ICON_COLORS = %w(blurple gold gray green magenta red) 
     def create
         @user = User.new(user_params)
+        color = ICON_COLORS.sample
+        file = File.open("frontend/src/assets/icons/avatar-#{color}.png")
+        @user.avatar.attach(io: file, filename: "#{color}.png")
         if @user.save
             log_in(@user)
             render :show
--- frontend/src/components/userIcon/userIcon.jsx
+++ frontend/src/components/userIcon/userIcon.jsx
@@ -1,20 +1,13 @@
 import './userIcon.css'
 
 const UserIcon = ({user, type}) => {
-    const defaultColors = ["blurple", "gold", "gray", "green", "magenta", "red"]
- // TODO: MOVE THIS LOGIC TO USERS CONTROLLER.
-    const assignImage = () => {
-        if (user.avatarUrl){
-            return user.avatarUrl
-        }else{
-            let color = defaultColors[Math.floor(Math.random() * 5)]
-            return `/src/assets/icons/avatar-${color}.png`
-        }
-    }
+
     return(
         <div className="user-icon">
-                <img className="avatar" src={assignImage()} />
+            <div className="icon-img">
+                <img className="avatar" src={user.avatarUrl} />
                 <img className='status' src="/src/assets/icons/icon-on.png" />
+            </div>
                 <div className="user-icon-text">
                     <h4>{user.username}</h4>
                     { type === 'bottom-left' && (<p>"status"</p>)}
